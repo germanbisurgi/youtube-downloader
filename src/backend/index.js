0d226e6b3273146db34b3c9e442af69cb2368d93
German Bisurgi <germanbisurgi@pop-os.localdomain>
2021-12-26 19:09:46 +0100

    refactoring

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,20 +1,23 @@
 const commandante = require('./commandante')
-const { app, BrowserWindow, ipcMain} = require('electron')
+const { app, BrowserWindow, ipcMain, dialog} = require('electron')
 const path = require('path')
-// require('electron-reloader')(module)
+require('electron-reloader')(module)
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 let win
 
 const createWindow = () => {
   win = new BrowserWindow({
+    height: 900,
+    width: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       // preload: path.join(__dirname, 'renderer.js')
     }
   })
-  win.webContents.openDevTools()
-  win.loadFile(path.join(__dirname, 'static', 'index.html'))
+  // win.webContents.openDevTools()
+  win.loadFile(path.join(__dirname, '..', 'frontend', 'index.html'))
 }
 
 app.whenReady().then(() => {
@@ -49,7 +52,8 @@ ipcMain.on('submit', (event, payload) => {
   ]
 
   const options = {
-    cwd: path.join(__dirname, 'output'),
+    // cwd: path.join(__dirname, 'output'),
+    cwd: app.getPath('videos')
   }
 
   let commandString = command + ' ' + args.join(' ')
